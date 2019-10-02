588a94eeb0006927469ea5648af8e859f3c12946
iq 18 <ivan.quirke@mycit.ie>
2019-10-02 20:26:43 +0100

    added Model and DbContext

---
+++ Contacts/Migrations/20191002192417_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Contacts.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    PMId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.PMId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ Contacts/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contacts.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int PMId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string PhoneNumber { get; set; } //MM/YY
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+    }
+}
